d9d6a6034ae2f25a38d3e0f6a7fcae96e2c1f222
Hsuan Lee <hsuan@cs.utah.edu>
2017-06-14 16:21:58 -0600

    login layout added

---
+++ apiAISampleApp/src/main/java/ai/api/sample/DataAsked.java
@@ -0,0 +1,12 @@
+package ai.api.sample;
+
+/**
+ * Created by u1076070 on 6/14/2017.
+ */
+
+//A Hashmap to save queries and answers which are already asked.
+
+public class DataAsked {
+
+    //TODO: load data from files
+}
