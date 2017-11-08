896bd84ff81da5284d165103ebc21143b2c039c5
Nithin Chalapathi <nithin.ch10@gmail.com>
2017-11-08 00:07:59 -0700

    Build a units hashmap for a list of units for speaking purposes

--- uteVoiceAgentApp/src/main/java/ute/webservice/voiceagent/Constants.java
+++ uteVoiceAgentApp/src/main/java/ute/webservice/voiceagent/Constants.java
@@ -1,5 +1,7 @@
 package ute.webservice.voiceagent;
 
+import java.util.HashMap;
+
 /**
  * Save global constant.
  * Created by u1076070 on 6/30/2017.
@@ -29,5 +31,34 @@ final class Constants {
     public final static String CLINWEB_CENSUS_QUERY = "https://clinweb.med.utah.edu/pricing-transparency-api/census/getCensus";
     public final static String CLINWEB_CENSUS_SPECFIC_QUERY = "https://clinweb.med.utah.edu/pricing-transparency-api/census/getCensusByUnit/";
 
+    public HashMap<String, String> units = new HashMap<String, String>();
+
+    public Constants(){
+        units.put("AIMA", "A I M A");
+        units.put("AIMB","A I M B");
+        units.put("BRN","B R N");
+        units.put("CVICU","C V I C U");
+        units.put("CVMU","C V M U");
+        units.put("HCBMT","H C B M T");
+        units.put("HCH4","H C H 4");
+        units.put("HCH5","H C H 5");
+        units.put("HCICU","H C I C U");
+        units.put("ICN","I C N");
+        units.put("IMR","I M R");
+        units.put("LND","L N D");
+        units.put("MICU","M I C U");
+        units.put("MNBC","M N B C");
+        units.put("NAC","N A C");
+        units.put("NCCU","N C C U");
+        units.put("NICU","N I C U");
+        units.put("NNCCN","N N C C N");
+        units.put("NSY","N S Y");
+        units.put("OBGY","O B G Y");
+        units.put("OTSS","O T S S");
+        units.put("SICU","S I C U");
+        units.put("SSTU","S S T U");
+        units.put("UUOC","U U O C");
+        units.put("GI", "G I");
+    }
 
 }
