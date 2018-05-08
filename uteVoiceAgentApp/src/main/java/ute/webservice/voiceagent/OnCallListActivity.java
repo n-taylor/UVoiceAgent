2dc0a783a8ba40599255f2a022b70cbd5d766e46
Nathan Taylor <natekeithtaylor@gmail.com>
2018-05-08 14:54:39 -0600

    Add the OnCallList Activity and set up its layout

---
+++ uteVoiceAgentApp/src/main/java/ute/webservice/voiceagent/OnCallListActivity.java
@@ -0,0 +1,13 @@
+package ute.webservice.voiceagent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OnCallListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_call_list);
+    }
+}
