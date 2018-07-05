9b0b668192287661d937833890fc88d212e00161
n-taylor <natekeithtaylor@gmail.com>
2018-07-05 11:48:45 -0600

    Remove unecessary code

--- uteVoiceAgentApp/src/main/java/ute/webservice/voiceagent/util/AccountCheck.java
+++ uteVoiceAgentApp/src/main/java/ute/webservice/voiceagent/util/AccountCheck.java
@@ -54,22 +54,9 @@ public class AccountCheck {
     private static final int timeout = 5000;
 
     private static final String TAG = "AccountCheck";
-    public static final String PEER_CERTIFICATES = "PEER_CERTIFICATES";
-
-    private HashMap<String,String> account_map = new HashMap<String, String>();
-    private HashMap<String,Integer> admin_map = new HashMap<String, Integer>();
 
     private String accountID;
-    private Constants const_value;
     public AccountCheck(){
-
-        account_map.put("account02","abcd");
-        account_map.put("account01","1234");
-        account_map.put("admin","admin");
-
-        admin_map.put("admin", Constants.ACCESS_LEVEL_ADMIN);
-        admin_map.put("account01", Constants.ACCESS_LEVEL_HIGH);
-        admin_map.put("account02", Constants.ACCESS_LEVEL_LOW);
     }
 
     /**
@@ -86,47 +73,6 @@ public class AccountCheck {
      */
     private void setAccountID(String inputID){
         accountID=inputID;
-    }
-
-    /**
-     * Return authorization level
-     * @return user's authorization
-     */
-    public int getAccessLevel(){
-        if(accountID!=null && !accountID.isEmpty())
-            return admin_map.get(accountID);
-        return -1;
-    }
-
-    /**
-     * Connect to account database, and then check if account exist and password is correct.
-     * @param ID Input account ID
-     * @param password Input password
-     * @return return true if all input information is right; else false.
-     */
-    public boolean isAccountCorrect(String ID, String password){
-        if(!account_map.containsKey(ID))
-            return false;
-        if(!account_map.get(ID).equals(password))
-            return false;
-
-        setAccountID(ID);
-        return true;
-    }
-
-    /**
-     * Connect to account database, and then check if account exist and password is correct.
-     * @param param
-     * @return
-     */
-    public boolean isAccountCorrect(String[] param){
-        if(param.length!=2 || !account_map.containsKey(param[0]))
-            return false;
-        if(!account_map.get(param[0]).equals(param[1]))
-            return false;
-
-        setAccountID(param[0]);
-        return true;
     }
 
 
